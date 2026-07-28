b4e068d3eb6447eb1339d7df846046c67f3c4c1f
adneyvadi-byte <adneyvadi@gmail.com>
2026-07-28 22:57:35 +0530

    Update books.js

--- books.js
+++ books.js
@@ -13,7 +13,7 @@ const books = [
         category: "Sant Sahitya",
         author: "Sant Dnyaneshwar",
         description: "Marathi commentary on the Bhagavad Gita.",
-        cover: "https://via.placeholder.com/150x220?text=Dnyaneshwari",
+        cover: "https://www.google.com/search?client=ms-android-vivo-terr1-rso2&hs=sc0&sca_esv=5e05885409c385ba&sxsrf=APpeQnuaO9B6YkkYbszIhKjxOVCF8IyUzA:1785259541869&udm=2&fbs=ABfTbFWX8Can4Rb7JjuFvBw79YrXuK78L_sHkEUm6BGU2Xt8MB1DoILzmSB4Yn2o139vCK2e3gonapooXVOEA3fRUwExu33wgP3imytGw_kr9SJRv03QDzf36Qkrgq9OIE77k4nnHGcrHcioXlPoYt8PNsFdAAGZeWXQ-flDLCO6D_IpFjRTXSxHfkvuAQ-ap0c8PXu-3zfMJEQDiUqZdUEYBlVty6E9td_IvE4HVlmJ87GAirXE0S8&q=dnyaneshwari&sa=X&sqi=2&ved=2ahUKEwjhtsyG8vWVAxVUkeEIHcowH3wQtKgLegQIFxAB#sv=CAMSVxoyKhBlLTl1TXRhU0M1RlBudXlNMg45dU10YVNDNUZQbnV5TToOMXk4Rjl6d05XRGUzNE0gBCoXCgFzEhBlLTl1TXRhU0M1RlBudXlNGAEwAUoECAEQAhgHILClzYoISggQAhgBIAIoAQ",
         link: "https://drive.google.com/file/d/1As6vnRfs4auzrphWwJxsbjl2pnSvTcq0/view?usp=drivesdk"
     },
 
